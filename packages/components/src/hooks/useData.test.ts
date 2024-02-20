18e35d41bcfd941203da4f98e6e09f9c0ac0d054
asimonok <sashasimonok@gmail.com>
2024-02-20 10:53:31 +0300

    Add useData hook

---
+++ packages/components/src/hooks/useData.test.ts
@@ -0,0 +1,156 @@
+import { act, renderHook } from '@testing-library/react';
+import { useCallback } from 'react';
+
+import { createUseDataHook } from './useData';
+
+/**
+ * Mock timers
+ */
+jest.useFakeTimers();
+
+/**
+ * With Latency
+ * @param result
+ * @param timeout
+ */
+const withLatency = <T>(result: T, timeout = 15) => {
+  return new Promise((resolve) => setTimeout(() => resolve(result), timeout));
+};
+
+describe('useData', () => {
+  const defaultDatasource = {
+    api: {
+      getAll: jest.fn(),
+    },
+  };
+
+  const useData = createUseDataHook<typeof defaultDatasource>();
+
+  it('Should have initial loading', async () => {
+    const datasource = {
+      ...defaultDatasource,
+      api: {
+        ...defaultDatasource.api,
+        getAll: jest.fn(() => withLatency([])),
+      },
+    };
+
+    const { result } = await act(async () =>
+      renderHook(() =>
+        useData({
+          datasource,
+          query: useCallback((datasource) => datasource.api.getAll(), []),
+          initial: [],
+        })
+      )
+    );
+
+    expect(result.current.loaded).toBeFalsy();
+    expect(result.current.data).toEqual([]);
+    expect(result.current.loading).toBeTruthy();
+    expect(result.current.error).toBeFalsy();
+  });
+
+  it('Should load data', async () => {
+    const datasource = {
+      ...defaultDatasource,
+      api: {
+        ...defaultDatasource.api,
+        getAll: jest.fn(() => [{ id: '1' }, { id: '2' }]),
+      },
+    };
+
+    const { result } = await act(async () =>
+      renderHook(() =>
+        useData({
+          datasource,
+          query: useCallback((datasource) => datasource.api.getAll(), []),
+          initial: [],
+        })
+      )
+    );
+
+    expect(result.current.data).toEqual([{ id: '1' }, { id: '2' }]);
+    expect(result.current.loading).toBeFalsy();
+    expect(result.current.error).toBeFalsy();
+    expect(result.current.loaded).toBeTruthy();
+  });
+
+  it('Should return error if no datasource', async () => {
+    const datasource = {
+      ...defaultDatasource,
+      api: {
+        ...defaultDatasource.api,
+        getAll: jest.fn(() => {
+          throw new Error();
+        }),
+      },
+    };
+
+    const { result } = await act(async () =>
+      renderHook(() =>
+        useData({
+          datasource,
+          query: useCallback((datasource) => datasource.api.getAll(), []),
+          initial: [],
+        })
+      )
+    );
+
+    expect(result.current.data).toEqual([]);
+    expect(result.current.loading).toBeFalsy();
+    expect(result.current.error).toBeTruthy();
+    expect(result.current.loaded).toBeFalsy();
+  });
+
+  it('Should return error if api error', async () => {
+    const { result } = await act(async () =>
+      renderHook(() =>
+        useData({
+          datasource: null,
+          query: useCallback((datasource) => datasource.api.getAll(), []),
+          initial: [],
+        })
+      )
+    );
+
+    expect(result.current.data).toEqual([]);
+    expect(result.current.loading).toBeFalsy();
+    expect(result.current.error).toBeTruthy();
+    expect(result.current.loaded).toBeFalsy();
+  });
+
+  it('Should refresh data', async () => {
+    const datasource = {
+      ...defaultDatasource,
+      api: {
+        ...defaultDatasource.api,
+        getAll: jest.fn<{ id: string }[], never>(() => [{ id: '1' }, { id: '2' }]),
+      },
+    };
+
+    const { result } = await act(async () =>
+      renderHook(() =>
+        useData({
+          datasource,
+          query: useCallback((datasource) => datasource.api.getAll(), []),
+          initial: [],
+        })
+      )
+    );
+
+    expect(result.current.data).toEqual([{ id: '1' }, { id: '2' }]);
+
+    jest.mocked(datasource.api.getAll).mockResolvedValue([{ id: '1' }]);
+
+    /**
+     * Refresh
+     */
+    await act(async () => result.current.update());
+
+    /**
+     * Check updated result
+     */
+    expect(result.current.data).toEqual([{ id: '1' }]);
+  });
+});
---
+++ packages/components/src/hooks/useData.ts
@@ -0,0 +1,91 @@
+import { useCallback, useEffect, useState } from 'react';
+
+/**
+ * Data Source With Api
+ */
+type DatasourceWithApi<TDataSource extends { api?: unknown }> = TDataSource & { api: Required<TDataSource>['api'] };
+
+/**
+ * Type for useCallback function
+ */
+type UseCallback<
+  TDataSource extends { api?: unknown },
+  T extends (datasource: DatasourceWithApi<TDataSource>) => ReturnType<T>,
+> = ((...args: Parameters<T>) => ReturnType<T>) & {
+  /**
+   * Specified property to check if wrapped in useCallback
+   */
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  __IS_USE_CALLBACK?: undefined;
+};
+
+/**
+ * Create Use Data Hook
+ */
+export const createUseDataHook = <TDataSource extends { api?: unknown }>() => {
+  return <TValue>({
+    datasource,
+    query,
+    initial,
+  }: {
+    datasource: TDataSource | null;
+    query: UseCallback<TDataSource, (datasource: TDataSource) => Promise<TValue | undefined>>;
+    initial: TValue;
+  }) => {
+    const [data, setData] = useState<TValue>(initial);
+    const [loading, setLoading] = useState(false);
+    const [loaded, setLoaded] = useState(false);
+    const [error, setError] = useState(false);
+
+    /**
+     * Load
+     */
+    const load = useCallback(async () => {
+      if (!datasource || !datasource.api) {
+        setError(true);
+        setLoaded(false);
+        return;
+      }
+
+      /**
+       * Start Loading
+       */
+      setLoading(true);
+      setError(false);
+      setLoaded(false);
+
+      try {
+        const data = await query({
+          ...datasource,
+          api: datasource.api,
+        });
+
+        if (data !== undefined) {
+          setData(data);
+          setLoading(false);
+          setLoaded(true);
+          setError(false);
+        }
+      } catch {
+        setLoading(false);
+        setError(true);
+        setLoaded(false);
+      }
+    }, [datasource, query]);
+
+    /**
+     * Load Data
+     */
+    useEffect(() => {
+      load();
+    }, [datasource, load, query]);
+
+    return {
+      data,
+      loading,
+      update: load,
+      error,
+      loaded,
+    };
+  };
+};
