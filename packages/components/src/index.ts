9f3091c84111ffcbe2b5fc58453db583c2caef4c
asimonok <sashasimonok@gmail.com>
2024-02-20 10:54:36 +0300

    Add exports

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,7 +1,7 @@
 import { TEST_IDS } from './constants';
 
 export * from './components';
-export { useFormBuilder } from './hooks';
+export { createUseDataHook, useFormBuilder } from './hooks';
 export * from './types';
 
 export const formSelectors = TEST_IDS.form;
