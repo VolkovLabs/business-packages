c026071f8d12c9c59e6faa21491ae67c274da4cc
Alex Simonok <sashasimonok@gmail.com>
2024-02-29 15:35:08 +0300

    Add Code Parameters Builder (#32)
    * Add Code Parameters Builder
    
    * Prepare components release 1.6.0

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -3,5 +3,6 @@ import { TEST_IDS } from './constants';
 export * from './components';
 export { createUseDataHook, useFormBuilder } from './hooks';
 export * from './types';
+export { CodeParameterItem, CodeParametersBuilder } from './utils';
 
 export const formSelectors = TEST_IDS.form;
