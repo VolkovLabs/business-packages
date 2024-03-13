2edb0d920efea1b2871a8397339e5a226e146556
Vitaly Pinchuk <146737590+vitPinchuk@users.noreply.github.com>
2024-03-13 23:44:04 +0300

    Add min and max heights to Autosize Code editor (#34)
    * min max Height added
    
    * Formatting
    
    * Update changelog
    
    * Add code coverage
    
    ---------
    
    Co-authored-by: asimonok <sashasimonok@gmail.com>
    Co-authored-by: Mikhail Volkov <mikhail@volkovlabs.io>

--- packages/components/src/components/AutosizeCodeEditor/AutosizeCodeEditor.tsx
+++ packages/components/src/components/AutosizeCodeEditor/AutosizeCodeEditor.tsx
@@ -6,21 +6,29 @@ import { CODE_EDITOR_CONFIG } from '../../constants';
 /**
  * Properties
  */
-type Props = React.ComponentProps<typeof CodeEditor>;
+type Props = React.ComponentProps<typeof CodeEditor> & {
+  minHeight?: number;
+  maxHeight?: number;
+};
 
 /**
  * Get Height By Value
  * @param value
+ * @param minHeight
+ * @param maxHeight
  */
-const getHeightByValue = (value: string) => {
+const getHeightByValue = (value: string, minHeight?: number, maxHeight?: number) => {
   const height = value.split('\n').length * CODE_EDITOR_CONFIG.lineHeight;
 
-  if (height < CODE_EDITOR_CONFIG.height.min) {
-    return CODE_EDITOR_CONFIG.height.min;
+  const minCurrentHeight = minHeight || CODE_EDITOR_CONFIG.height.min;
+  const maxCurrentHeight = maxHeight || CODE_EDITOR_CONFIG.height.max;
+
+  if (height < minCurrentHeight) {
+    return minCurrentHeight;
   }
 
-  if (height > CODE_EDITOR_CONFIG.height.max) {
-    return CODE_EDITOR_CONFIG.height.max;
+  if (height > maxCurrentHeight) {
+    return maxCurrentHeight;
   }
 
   return height;
@@ -30,25 +38,32 @@ const getHeightByValue = (value: string) => {
  * Autosize Code Editor
  * @constructor
  */
-export const AutosizeCodeEditor: React.FC<Props> = ({ value, onChange, height: staticHeight, ...restProps }) => {
+export const AutosizeCodeEditor: React.FC<Props> = ({
+  value,
+  onChange,
+  minHeight,
+  maxHeight,
+  height: staticHeight,
+  ...restProps
+}) => {
   /**
    * Height
    */
-  const [height, setHeight] = useState(getHeightByValue(value));
+  const [height, setHeight] = useState(getHeightByValue(value, minHeight, maxHeight));
 
   /**
    * Update Height on value change
    */
   useEffect(() => {
-    setHeight(getHeightByValue(value));
-  }, [value]);
+    setHeight(getHeightByValue(value, minHeight, maxHeight));
+  }, [value, minHeight, maxHeight]);
 
   return (
     <CodeEditor
       value={value}
       onChange={(value) => {
         onChange?.(value);
-        setHeight(getHeightByValue(value));
+        setHeight(getHeightByValue(value, minHeight, maxHeight));
       }}
       height={staticHeight ?? height}
       {...restProps}
