9572edb28a554c212c12fbee590fec9b4f6a0b50
asimonok <sashasimonok@gmail.com>
2024-06-20 12:07:10 +0300

    Add build for server and client environments to reduce bundle size

---
+++ packages/grafana-utils/src/client.ts
@@ -0,0 +1,7 @@
+export * from './constants';
+export * from './grafana/types';
+export * from './privateTransformers';
+export * from './query';
+export * from './types';
+export * from './utils';
+export * from './variables';
