369bcccd7fb5bca21e1e3e684473d2d8f1858605
Mikhail Volkov <mikhail@volkovlabs.io>
2025-07-31 09:22:10 -0400

    Update to Node 22

--- packages/jest-selectors/rollup.config.mjs
+++ packages/jest-selectors/rollup.config.mjs
@@ -1,27 +1,10 @@
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
-import packageJson from './package.json' assert { type: 'json' };
+import packageJson from './package.json' with { type: 'json' };
 
 const name = packageJson.main.replace(/\.js$/, '');
 
 export default [
-  {
-    input: `src/index.ts`,
-    plugins: [esbuild()],
-    output: [
-      {
-        file: `${name}.js`,
-        format: 'cjs',
-        sourcemap: true,
-      },
-    ],
-  },
-  {
-    input: `src/index.ts`,
-    plugins: [dts()],
-    output: {
-      file: `${name}.d.ts`,
-      format: 'es',
-    },
-  },
+  { input: `src/index.ts`, plugins: [esbuild()], output: [{ file: `${name}.js`, format: 'cjs', sourcemap: true }] },
+  { input: `src/index.ts`, plugins: [dts()], output: { file: `${name}.d.ts`, format: 'es' } },
 ];
